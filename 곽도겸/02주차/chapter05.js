53f5a01475da991d95011ab9d80ce7034a06c60d
곽도겸 <zooc76762@gmail.com>
2026-09-15 04:26:54 +0900

    feat: 02주차 코드정리

---
+++ 곽도겸/02주차/chapter05.js
@@ -0,0 +1,27 @@
+// 1. Number Type
+let num1 =27;
+let num2 =1.5;
+let num3 =-20;
+
+let inf = Infinity;
+let minf = -Infinity;
+
+let nan = NaN;
+
+// 2. String Type
+let myName = "곽도겸";
+let myLocation = "인천";
+let introduce = myName + myLocation;
+
+//backtick을 사용해서 문자열을 만들면 달러사인과 중괄호를 통해서 변수값을 동적으로 문자열에 포함시킬 수 있다 이와 같은 문법을 템플릿 리터럴 문법이라고 한다.
+let introduceText = `${myName}은 ${myLocation}에 거주한다`;
+
+// 3.Boolean Type
+let isSwitchOn = false;
+let isEmpty = false;
+
+// 4. Null Type (아무것도 없다)
+let empty = null;
+
+// 5. Undefiend Type
+let none;
