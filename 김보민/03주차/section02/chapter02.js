cbfb35f8d3c65776c5e34d0905c8587c97a30c5e
김보민 <bwuping@gmail.com>
2026-09-22 20:38:31 +0900

    3주차과제완료

---
+++ 김보민/03주차/section02/chapter02.js
@@ -0,0 +1,11 @@
+function returnFalse() {
+    console.log("False 함수");
+    return undefined;
+}
+
+function returnTrue() {
+    console.log("True 함수");
+    return 10;
+}
+
+console.log(returnFalse() && returnTrue());
